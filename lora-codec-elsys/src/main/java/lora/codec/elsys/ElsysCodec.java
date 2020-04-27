fd95ff5561ee100a2c6697e1560dc76d1c10041e
sagfcp <sagfcp@SAG-7T4RQF2.eur.ad.sag>
2020-04-27 23:41:45 +0200

    Added basic Elsys devices support (still missing a few payload types, to lazy to implement them all)

---
+++ lora-codec-elsys/src/main/java/lora/codec/elsys/ElsysCodec.java
@@ -0,0 +1,163 @@
+package lora.codec.elsys;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.cumulocity.rest.representation.inventory.ManagedObjectRepresentation;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.BaseEncoding;
+
+import lora.codec.C8YData;
+import lora.codec.DeviceCodec;
+import lora.codec.DeviceOperation;
+import lora.codec.DeviceOperationParam;
+import lora.codec.DeviceOperationParam.ParamType;
+import lora.codec.DownlinkData;
+
+@Component
+public class ElsysCodec extends DeviceCodec {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private List<String> models = new ArrayList<String>();
+
+	@Override
+	public String getId() {
+		return "elsys";
+	}
+
+	@Override
+	public String getName() {
+		return "Elsys";
+	}
+
+	@Override
+	public String getVersion() {
+		return "1.0";
+	}
+	
+	enum TYPE {
+		TEMPERATURE((byte)0x01) {
+			@Override
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+				BigDecimal temp = BigDecimal.valueOf(buffer.getShort()).divide(BigDecimal.valueOf(10));
+				c8yData.addMeasurement(mor, "c8y_TemperatureMeasurement", "T", "°C", temp, getTime(buffer, offsetSize));
+			}
+		},
+		HUMIDITY((byte)0x02) {
+
+			@Override
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+				BigDecimal humidity = BigDecimal.valueOf(buffer.get());
+				c8yData.addMeasurement(mor, "Humidity", "H", "%RH", humidity, getTime(buffer, offsetSize));
+			}
+		},
+		LIGHT((byte)0x04) {
+
+			@Override
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+				BigDecimal light = BigDecimal.valueOf(buffer.getShort());
+				c8yData.addMeasurement(mor, "Light", "L", "Lux", light, getTime(buffer, offsetSize));
+			}
+		},
+		MOTION((byte)0x05) {
+
+			@Override
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+				BigDecimal motion = BigDecimal.valueOf(buffer.get());
+				c8yData.addMeasurement(mor, "Motion", "count", "", motion, getTime(buffer, offsetSize));
+			}
+		},
+		BATTERY((byte)0x07) {
+
+			@Override
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+				BigDecimal battery = BigDecimal.valueOf(buffer.getShort());
+				c8yData.addMeasurement(mor, "Battery", "power", "mV", battery, getTime(buffer, offsetSize));
+			}
+		};
+		
+		byte value;
+		int size;
+		
+		private TYPE(byte value) {
+			this.value = value;
+		}
+		static final Map<Byte, TYPE> BY_VALUE = new HashMap<>();
+
+		static {
+			for (TYPE f : values()) {
+				BY_VALUE.put(f.value, f);
+			}
+		}
+
+		private static DateTime getTime(ByteBuffer buffer, int offsetSize) {
+			long offset = offsetSize == 4 ? buffer.getInt() : offsetSize == 2 ? buffer.getShort() : offsetSize == 1 ? buffer.get() : 0;
+			DateTime time = new DateTime().minus(offset*1000);
+			return time;
+		}
+		
+		public abstract void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize);
+	}
+
+	@Override
+	protected C8YData decode(ManagedObjectRepresentation mor, String model, int fport, DateTime updateTime, byte[] payload) {
+		C8YData c8yData = new C8YData();
+		
+		ByteBuffer buffer = ByteBuffer.wrap(payload);
+		while (buffer.hasRemaining()) {
+			byte type = buffer.get();
+			int offsetSize = (type >> 6) & 0x03;
+			type = (byte)(type & 0x3f);
+			TYPE.BY_VALUE.get(type).process(mor, buffer, c8yData, offsetSize);
+		}
+		
+		return c8yData;
+	}
+
+	@Override
+	public List<String> getModels() {
+		return models;
+	}
+
+	@Override
+	protected DownlinkData encode(ManagedObjectRepresentation mor, String model, String operation) {
+		String payload = null;
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode root = mapper.readTree(operation);
+			byte[] bytes = new byte[11];
+			payload = BaseEncoding.base16().encode(bytes);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new DownlinkData(null, 1, payload);
+	}
+
+	@Override
+	public DownlinkData askDeviceConfig(String devEui) {
+		return null;
+	}
+
+
+	@Override
+	public Map<String, DeviceOperation> getAvailableOperations(String model) {
+		Map<String, DeviceOperation> result = new HashMap<String, DeviceOperation>();
+		
+		List<DeviceOperationParam> params = new ArrayList<DeviceOperationParam>();
+		
+		return result;
+	}
+
+}
---
+++ lora-codec-elsys/src/test/java/lora/codec/elsys/TestDecode.java
@@ -0,0 +1,40 @@
+package lora.codec.elsys;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+
+import org.joda.time.DateTime;
+import org.junit.jupiter.api.Test;
+
+import com.cumulocity.model.measurement.MeasurementValue;
+import com.cumulocity.rest.representation.inventory.ManagedObjectRepresentation;
+import com.cumulocity.rest.representation.measurement.MeasurementRepresentation;
+import com.google.common.io.BaseEncoding;
+
+import lora.codec.C8YData;
+
+public class TestDecode {
+	
+	@Test
+	public void testPayload() {
+		byte[] payload = BaseEncoding.base16().decode("010100022e040001050b070e41".toUpperCase());
+
+		ElsysCodec codec = new ElsysCodec();
+
+		C8YData c8yData = codec.decode(new ManagedObjectRepresentation(), "-", 1, new DateTime(), payload);
+		
+		System.out.println(c8yData.getMeasurements().iterator().next().toJSON());
+		
+		assertEquals(5, c8yData.getMeasurements().size());
+		
+		for (MeasurementRepresentation m: c8yData.getMeasurements()) {
+			if (m.getType().equals("c8y_TemperatureMeasurement")) {
+				assertEquals(25.6, ((Map<String, MeasurementValue>)m.get("c8y_TemperatureMeasurement")).get("T").getValue().doubleValue());
+			}
+			if (m.getType().equals("Humidity")) {
+				assertEquals(46, ((Map<String, MeasurementValue>)m.get("Humidity")).get("H").getValue().intValue());
+			}
+		}
+	}
+}
