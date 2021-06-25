dcf5bd818aa23e766a942b5c4a0d77d15488610a
haraldmeyer <harald.meyer@softwareag.com>
2021-06-25 09:16:41 +0200

    support to skip downlinks but still have the C8Y operation pass

--- lora-interface/src/main/java/lora/codec/DownlinkData.java
+++ lora-interface/src/main/java/lora/codec/DownlinkData.java
@@ -4,12 +4,14 @@ public class DownlinkData {
 	private String devEui;
 	private Integer fport;
 	private String payload;
+	private boolean skipDownlink;
 	public DownlinkData() {}
 	public DownlinkData(String devEui, Integer fport, String payload) {
 		super();
 		this.devEui = devEui;
 		this.fport = fport;
 		this.payload = payload;
+		this.setSkipDownlink(false);
 	}
 	public Integer getFport() {
 		return fport;
@@ -29,8 +31,14 @@ public class DownlinkData {
 	public void setDevEui(String devEui) {
 		this.devEui = devEui;
 	}
+	public boolean isSkipDownlink() {
+		return skipDownlink;
+	}
+	public void setSkipDownlink(boolean skipDownlink) {
+		this.skipDownlink = skipDownlink;
+	}
 	@Override
 	public String toString() {
-		return "DownlinkData [devEui=" + devEui + ", fport=" + fport + ", payload=" + payload + "]";
+		return "DownlinkData [devEui=" + devEui + ", fport=" + fport + ", payload=" + payload + ", skipDownlink=" + skipDownlink+ "]";
 	}
 }
