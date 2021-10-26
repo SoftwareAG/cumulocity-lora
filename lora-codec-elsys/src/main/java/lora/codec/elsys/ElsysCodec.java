a12ed453ea2fc537c8aa5fa678f9c6f58a928a63
cpoder <cyril.poder@gmail.com>
2021-10-26 16:34:32 +0200

    added support for event time

--- lora-codec-elsys/src/main/java/lora/codec/elsys/ElsysCodec.java
+++ lora-codec-elsys/src/main/java/lora/codec/elsys/ElsysCodec.java
@@ -46,57 +46,57 @@ public class ElsysCodec extends DeviceCodec {
 	enum TYPE {
 		TEMPERATURE((byte)0x01) {
 			@Override
-			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, DateTime time, int offsetSize) {
 				BigDecimal temp = BigDecimal.valueOf(buffer.getShort()).divide(BigDecimal.valueOf(10));
-				c8yData.addMeasurement(mor, "c8y_TemperatureMeasurement", "T", "°C", temp, getTime(buffer, offsetSize));
+				c8yData.addMeasurement(mor, "c8y_TemperatureMeasurement", "T", "°C", temp, getTime(buffer, time, offsetSize));
 			}
 		},
 		HUMIDITY((byte)0x02) {
 
 			@Override
-			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, DateTime time, int offsetSize) {
 				BigDecimal humidity = BigDecimal.valueOf(buffer.get());
-				c8yData.addMeasurement(mor, "Humidity", "H", "%RH", humidity, getTime(buffer, offsetSize));
+				c8yData.addMeasurement(mor, "Humidity", "H", "%RH", humidity, getTime(buffer, time, offsetSize));
 			}
 		},
 		LIGHT((byte)0x04) {
 
 			@Override
-			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, DateTime time, int offsetSize) {
 				BigDecimal light = BigDecimal.valueOf(buffer.getShort());
-				c8yData.addMeasurement(mor, "Light", "L", "Lux", light, getTime(buffer, offsetSize));
+				c8yData.addMeasurement(mor, "Light", "L", "Lux", light, getTime(buffer, time, offsetSize));
 			}
 		},
 		MOTION((byte)0x05) {
 
 			@Override
-			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, DateTime time, int offsetSize) {
 				BigDecimal motion = BigDecimal.valueOf(buffer.get());
-				c8yData.addMeasurement(mor, "Motion", "count", "", motion, getTime(buffer, offsetSize));
+				c8yData.addMeasurement(mor, "Motion", "count", "", motion, getTime(buffer, time, offsetSize));
 			}
 		},
         CO2((byte)0x06) {
 
             @Override
-            public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+            public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, DateTime time, int offsetSize) {
                 BigDecimal motion = BigDecimal.valueOf(buffer.getShort());
-                c8yData.addMeasurement(mor, "CO2", "ppm", "", motion, getTime(buffer, offsetSize));
+                c8yData.addMeasurement(mor, "CO2", "ppm", "", motion, getTime(buffer, time, offsetSize));
             }
         },
 		BATTERY((byte)0x07) {
 
 			@Override
-			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+			public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, DateTime time, int offsetSize) {
 				BigDecimal battery = BigDecimal.valueOf(buffer.getShort());
-				c8yData.addMeasurement(mor, "Battery", "power", "mV", battery, getTime(buffer, offsetSize));
+				c8yData.addMeasurement(mor, "Battery", "power", "mV", battery, getTime(buffer, time, offsetSize));
 			}
 		},
         PULSECOUNT2((byte)0x10) {
 
             @Override
-            public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize) {
+            public void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, DateTime time, int offsetSize) {
                 BigDecimal pc2 = BigDecimal.valueOf(buffer.getShort());
-                c8yData.addMeasurement(mor, "Pulse Count", "count", "", pc2, getTime(buffer, offsetSize));
+                c8yData.addMeasurement(mor, "Pulse Count", "count", "", pc2, getTime(buffer, time, offsetSize));
             }
         };
 		
@@ -114,13 +114,12 @@ public class ElsysCodec extends DeviceCodec {
 			}
 		}
 
-		private static DateTime getTime(ByteBuffer buffer, int offsetSize) {
+		private static DateTime getTime(ByteBuffer buffer, DateTime time, int offsetSize) {
 			long offset = offsetSize == 4 ? buffer.getInt() : offsetSize == 2 ? buffer.getShort() : offsetSize == 1 ? buffer.get() : 0;
-			DateTime time = new DateTime().minus(offset*1000);
-			return time;
+			return time.minus(offset*1000);
 		}
 		
-		public abstract void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, int offsetSize);
+		public abstract void process(ManagedObjectRepresentation mor, ByteBuffer buffer, C8YData c8yData, DateTime time, int offsetSize);
 	}
 
 	@Override
@@ -132,7 +131,7 @@ public class ElsysCodec extends DeviceCodec {
 			byte type = buffer.get();
 			int offsetSize = (type >> 6) & 0x03;
 			type = (byte)(type & 0x3f);
-			TYPE.BY_VALUE.get(type).process(mor, buffer, c8yData, offsetSize);
+			TYPE.BY_VALUE.get(type).process(mor, buffer, c8yData, new DateTime(decode.getUpdateTime()), offsetSize);
 		}
 		
 		return c8yData;
