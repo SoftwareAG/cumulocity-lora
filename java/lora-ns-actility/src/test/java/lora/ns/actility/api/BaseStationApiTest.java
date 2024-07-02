9a7858735725d2acd228f604ce84cd49e347b779
cpoder <cyril.poder@gmail.com>
2024-07-02 15:04:56 +0200

    completed test for basestation

--- java/lora-ns-actility/src/test/java/lora/ns/actility/api/BaseStationApiTest.java
+++ java/lora-ns-actility/src/test/java/lora/ns/actility/api/BaseStationApiTest.java
@@ -13,75 +13,77 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lora.ns.actility.api.model.basestation.BsBriefInt.TypeEnum;
+import lora.ns.actility.api.model.basestation.BsBriefIntEthernet;
 import lora.ns.actility.api.model.basestation.Bss;
 
 @ExtendWith(MockitoExtension.class)
 class BaseStationApiTest {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+	private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Mock
-    BaseStationApi baseStationApi;
+	@Mock
+	BaseStationApi baseStationApi;
 
-    @Test
-    void test_receive_basetation_with_ethernet_type() throws JsonMappingException, JsonProcessingException {
-        String bssJson = """
-                        {
-                        	"briefs": [
-                        		{
-                        			"name": "FR_PARIS_DF_R&D_4",
-                        			"model": {
-                        				"commercialName": "Blue Conduit V1.5 with GPS",
-                        				"ID": "MULT/MTAC.3",
-                        				"logo": "/thingpark/wireless/rest/resources/files/logo/baseStationProfile/4775"
-                        			},
-                        			"lrrID": "10003F42",
-                        			"power": {
-                        				"status": null,
-                        				"batteryLevel": null
-                        			},
-                        			"upFrame": {
-                        				"last": {
-                        					"date": 1679984083404,
-                        					"lc": "LC2",
-                        					"ant": "A1"
-                        				},
-                        				"history": {},
-                        				"minRC": {
-                        					"rc": null,
-                        					"lc": null,
-                        					"ant": null
-                        				}
-                        			},
-                        			"dwFrame": {
-                        				"last": {
-                        					"date": 1679945625851,
-                        					"lc": "LC8",
-                        					"ant": "A1"
-                        				},
-                        				"minRC": {
-                        					"rc": null,
-                        					"lc": null,
-                        					"ant": null
-                        				}
-                        			},
-                        			"ints": {
-                        				"int": [
-                        					{
-                        						"name": "eth0",
-                        						"type": "ETHERNET",
-                        						"state": null
-                        					}
-                        				]
-                        			}
-                        		}
-                        	]
-                        }""";
-        var bss = objectMapper.readValue(bssJson, Bss.class);
-        when(baseStationApi.getBaseStations(1)).thenReturn(bss);
-        var result = baseStationApi.getBaseStations(1);
-        assertThat(result.getBriefs()).hasSize(1);
-        assertThat(result.getBriefs().get(0).getInts().getInt()).hasSize(1);
-        assertThat(result.getBriefs().get(0).getInts().getInt().get(0).getType()).isEqualTo(TypeEnum.ETHERNET);
-    }
+	@Test
+	void test_receive_basetation_with_ethernet_type() throws JsonMappingException, JsonProcessingException {
+		String bssJson = """
+						{
+							"briefs": [
+								{
+									"name": "FR_PARIS_DF_R&D_4",
+									"model": {
+										"commercialName": "Blue Conduit V1.5 with GPS",
+										"ID": "MULT/MTAC.3",
+										"logo": "/thingpark/wireless/rest/resources/files/logo/baseStationProfile/4775"
+									},
+									"lrrID": "10003F42",
+									"power": {
+										"status": null,
+										"batteryLevel": null
+									},
+									"upFrame": {
+										"last": {
+											"date": 1679984083404,
+											"lc": "LC2",
+											"ant": "A1"
+										},
+										"history": {},
+										"minRC": {
+											"rc": null,
+											"lc": null,
+											"ant": null
+										}
+									},
+									"dwFrame": {
+										"last": {
+											"date": 1679945625851,
+											"lc": "LC8",
+											"ant": "A1"
+										},
+										"minRC": {
+											"rc": null,
+											"lc": null,
+											"ant": null
+										}
+									},
+									"ints": {
+										"int": [
+											{
+												"name": "eth0",
+												"type": "ETHERNET",
+												"state": null
+											}
+										]
+									}
+								}
+							]
+						}""";
+		var bss = objectMapper.readValue(bssJson, Bss.class);
+		when(baseStationApi.getBaseStations(1)).thenReturn(bss);
+		var result = baseStationApi.getBaseStations(1);
+		assertThat(result.getBriefs()).hasSize(1);
+		assertThat(result.getBriefs().get(0).getInts().getInt()).hasSize(1);
+		assertThat(result.getBriefs().get(0).getInts().getInt().get(0).getType()).isEqualTo(TypeEnum.ETHERNET);
+		assertThat(result.getBriefs().get(0).getInts().getInt().get(0)).isInstanceOf(BsBriefIntEthernet.class);
+	}
 }
